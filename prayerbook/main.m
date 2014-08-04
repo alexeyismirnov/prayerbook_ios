7d7c02765aeab089afea94039b1752b809540670
Alexey Smirnov <alexey@gmail.com>
2014-08-04 10:00:47 +0800

    Initial Commit

---
+++ prayerbook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  prayerbook
+//
+//  Created by Alexey Smirnov on 8/4/14.
+//  Copyright (c) 2014 Alexey Smirnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PrayerbookAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PrayerbookAppDelegate class]));
+    }
+}
