2a4ba4656430442c7e75aa656db6bac263737e57
Alexey Smirnov <alexey@gmail.com>
2014-12-02 12:08:14 +0800

    Added Mark, Luke, John books, added script.

---
+++ populate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import re
+import pprint as p
+import sqlite3 as lite
+
+with open('./matthew.txt', 'r') as content_file, lite.connect('./matthew.sqlite') as con:
+    content = content_file.read()
+    content = ' '.join(content.split())
+    
+    cur = con.cursor() 
+
+    cur.execute("DROP TABLE IF EXISTS gospel")
+    cur.execute("CREATE TABLE gospel(chapter INT,verse INT, text TEXT)")
+
+    chapter = 1
+    verse = 1
+    while True:
+        
+        pattern = ".*%d:%d (.*) %d:%d " % (chapter, verse, chapter, verse+1)
+        print "%d:%d" % (chapter, verse)
+        old_chapter = chapter
+        old_verse = verse
+                
+        obj = re.match(pattern, content)
+        if not obj:
+
+            pattern = ".*%d:%d (.*) %d:%d " % (chapter, verse, chapter+1, 1)
+            obj = re.match(pattern, content)
+            if not obj:
+                break
+
+            else:
+                chapter += 1
+                verse = 1
+                
+        else:
+            verse += 1
+
+        cur.execute("INSERT INTO gospel VALUES(%d, %d, \"%s\")" % (old_chapter, old_verse, obj.group(1)))  
+        p.pprint(obj.group(1))
+        
+    
+    pattern = ".*%d:%d (.*)" % (chapter, verse)
+    obj = re.match(pattern, content)
+    if obj:
+        cur.execute("INSERT INTO gospel VALUES(%d, %d, \"%s\")" % (chapter, verse, obj.group(1)))  
+        p.pprint(obj.group(1))
+        
