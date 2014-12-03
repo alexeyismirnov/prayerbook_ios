2c1a42356f213a9dc284a37c07aac15d41db4ba3
Alexey Smirnov <alexey@gmail.com>
2014-12-03 14:09:14 +0800

    Forgot to add Extensions

---
+++ prayerbook/Extensions.swift
@@ -0,0 +1,181 @@
+//
+//  Extensions.swift
+//  prayerbook
+//
+//  Created by Alexey Smirnov on 03.12.14.
+//  Copyright (c) 2014 Alexey Smirnov. All rights reserved.
+//
+
+import UIKit
+
+enum TimeIntervalUnit {
+    case Seconds, Minutes, Hours, Days, Months, Years
+    
+    func dateComponents(interval: Int) -> NSDateComponents {
+        var components:NSDateComponents = NSDateComponents()
+        
+        switch (self) {
+        case .Seconds:
+            components.second = interval
+        case .Minutes:
+            components.minute = interval
+        case .Days:
+            components.day = interval
+        case .Months:
+            components.month = interval
+        case .Years:
+            components.year = interval
+        default:
+            components.day = interval
+        }
+        return components
+    }
+}
+
+struct TimeInterval {
+    var interval: Int
+    var unit: TimeIntervalUnit
+    
+    init(interval: Int, unit: TimeIntervalUnit) {
+        self.interval = interval
+        self.unit = unit
+    }
+}
+
+// FYI: http://stackoverflow.com/questions/24116271/whats-the-cleanest-way-of-applying-map-to-a-dictionary-in-swift
+
+extension Int {
+    var days: TimeInterval {
+        return TimeInterval(interval: self, unit: TimeIntervalUnit.Days);
+    }
+}
+
+func - (let left:NSDate, let right:TimeInterval) -> NSDate {
+    let calendar = NSCalendar.currentCalendar()
+    var components = right.unit.dateComponents(-right.interval)
+    return calendar.dateByAddingComponents(components, toDate: left, options: nil)!
+}
+
+func + (let left:NSDate, let right:TimeInterval) -> NSDate {
+    let calendar = NSCalendar.currentCalendar()
+    var components = right.unit.dateComponents(right.interval)
+    return calendar.dateByAddingComponents(components, toDate: left, options: nil)!
+}
+
+extension NSDateComponents {
+    convenience init(_ day: Int, _ month:Int, _ year: Int) {
+        self.init()
+        
+        self.day = day
+        self.month = month
+        self.year = year
+    }
+    
+    convenience init(date: NSDate) {
+        self.init()
+        
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components(.CalendarUnitDay | .CalendarUnitMonth | .CalendarUnitYear | .CalendarUnitWeekday, fromDate: date)
+        
+        self.day = dateComponents.day
+        self.month = dateComponents.month
+        self.year = dateComponents.year
+        self.weekday = dateComponents.weekday
+    }
+    
+    func toDate() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        return calendar.dateFromComponents(self)!
+    }
+}
+
+func + (str: String, date: NSDate) -> String {
+    var formatter = NSDateFormatter()
+    formatter.dateStyle = .ShortStyle
+    formatter.timeStyle = .NoStyle
+    
+    return formatter.stringFromDate(date)
+}
+
+func + (arg1: NSMutableAttributedString?, arg2: NSMutableAttributedString?) -> NSMutableAttributedString? {
+    
+    if let rightArg = arg2 {
+        if let leftArg = arg1 {
+            var result = NSMutableAttributedString(attributedString: leftArg)
+            result.appendAttributedString(rightArg)
+            return result
+            
+        } else {
+            return arg2
+        }
+        
+    } else {
+        return arg1
+    }
+    
+}
+
+func + (arg1: NSMutableAttributedString?, arg2: String?) -> NSMutableAttributedString? {
+    
+    if let rightArg = arg2 {
+        if let leftArg = arg1 {
+            var result = NSMutableAttributedString(attributedString: leftArg)
+            result.appendAttributedString(NSMutableAttributedString(string: rightArg))
+            return result
+            
+        } else {
+            return NSMutableAttributedString(string: rightArg)
+        }
+        
+    } else {
+        return arg1
+    }
+}
+
+func + (arg1: NSMutableAttributedString?, arg2: (String?, UIColor)) -> NSMutableAttributedString? {
+    
+    if let rightArg = arg2.0 {
+        if let leftArg = arg1 {
+            var result = NSMutableAttributedString(attributedString: leftArg)
+            result.appendAttributedString(NSMutableAttributedString(string: rightArg, attributes: [NSForegroundColorAttributeName: arg2.1]))
+            return result
+            
+        } else {
+            return NSMutableAttributedString(string: rightArg, attributes: [NSForegroundColorAttributeName: arg2.1])
+        }
+        
+    } else {
+        return arg1
+    }
+}
+
+func += <K,V> (inout left: Dictionary<K, [V]>, right: Dictionary<K, [V]>) {
+    for (k, v) in right {
+        if let leftValue = left[k] {
+            left.updateValue(v + leftValue, forKey: k)
+        } else {
+            left.updateValue(v, forKey: k)
+        }
+    }
+}
+
+extension NSDate: Comparable {
+    
+}
+
+public func < (let left:NSDate, let right: NSDate) -> Bool {
+    var result:NSComparisonResult = left.compare(right)
+    return (result == .OrderedAscending)
+}
+
+public func == (let left:NSDate, let right: NSDate) -> Bool {
+    var result:NSComparisonResult = left.compare(right)
+    return (result == .OrderedSame)
+}
+
+extension String {
+    subscript (i: Int) -> String {
+        return String(Array(self)[i])
+    }
+}
+
