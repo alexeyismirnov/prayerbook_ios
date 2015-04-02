c13c48f26d900ac722233da46115d90b74477417
Alexey Smirnov <alexey@gmail.com>
2015-04-02 17:42:22 +0800

    Added dynamic ImageCell

--- prayerbook/DailyTab.swift
+++ prayerbook/DailyTab.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class DailyTab: UITableViewController {
+    let textCell = "TextCell"
+    let imageCell = "ImageCell"
+    
+    var fasting: (FastingType, String) = (.Vegetarian, "")
+    var foodIcon: [FastingType: String] = [
+        .NoFast:        "meat",
+        .Vegetarian:    "vegetables",
+        .FishAllowed:   "fish",
+        .FastFree:      "cupcake",
+        .Cheesefare:    "cheese",
+    ]
 
     var currentDate: NSDate = {
         // this is done to remove time component from date
@@ -34,15 +45,44 @@ class DailyTab: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2;
+        return 3;
+    }
+    
+    func getImageCell() -> ImageCell {
+        if let newCell = tableView.dequeueReusableCellWithIdentifier(imageCell) as? ImageCell {
+            return newCell
+
+        } else {
+            return UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: imageCell) as ImageCell
+        }
+    }
+    
+    func getTextCell() -> TextCell {
+        if let newCell  = tableView.dequeueReusableCellWithIdentifier(textCell) as? TextCell {
+            return newCell
+            
+        } else {
+            return UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: textCell) as TextCell
+            
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var newCell  = tableView.dequeueReusableCellWithIdentifier("TextCell") as TextCell!
+        
+        NSLog("index \(indexPath.row)")
 
-        configureTextCell(newCell, indexPath: indexPath)
+        switch indexPath.row {
+        case 2:
+            var newCell  = getImageCell()
+            configureImageCell(newCell, indexPath: indexPath)
+            return newCell
 
-        return newCell
+        default:
+            var newCell = getTextCell()
+            configureTextCell(newCell, indexPath: indexPath)
+            return newCell
+        }
+
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -54,7 +94,12 @@ class DailyTab: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return heightForTextCell(indexPath)
+        switch indexPath.row {
+        case 2:
+            return heightForImageCell(indexPath)
+        default:
+            return heightForTextCell(indexPath)
+        }
     }
     
     func configureTextCell(cell: TextCell, indexPath: NSIndexPath) {
@@ -65,7 +110,17 @@ class DailyTab: UITableViewController {
             cell.title.text = "qq qqq qq qqq qqq qqq qqq qqq qqq very very very very long long long long long text"
             
         }
-
+    }
+    
+    func configureImageCell(cell: ImageCell!, indexPath:NSIndexPath) {
+        
+        if let _fasting = Cal.getFastingDescription(currentDate) {
+            fasting = _fasting
+            let allowedFood = foodIcon[fasting.0]!
+            cell.title.text = fasting.1
+            cell.icon.image = UIImage(named: "food-\(allowedFood)")
+        }
+        
     }
     
     func heightForTextCell(indexPath: NSIndexPath) -> CGFloat {
@@ -75,10 +130,24 @@ class DailyTab: UITableViewController {
         }
         
         dispatch_once(&Static.onceToken) {
-            Static.cell = self.tableView.dequeueReusableCellWithIdentifier("TextCell") as TextCell!
+            Static.cell = self.getTextCell()
         }
         
         configureTextCell(Static.cell, indexPath: indexPath)
+        return calculateHeightForCell(Static.cell)
+    }
+
+    func heightForImageCell(indexPath: NSIndexPath) -> CGFloat {
+        struct Static {
+            static var cell : ImageCell!
+            static var onceToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.onceToken) {
+            Static.cell = self.getImageCell()
+        }
+        
+        configureImageCell(Static.cell, indexPath: indexPath)
         return calculateHeightForCell(Static.cell)
     }
 
---
+++ prayerbook/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCell.swift
+//  prayerbook
+//
+//  Created by Alexey Smirnov on 30.03.15.
+//  Copyright (c) 2015 Alexey Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell : UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    
+}
