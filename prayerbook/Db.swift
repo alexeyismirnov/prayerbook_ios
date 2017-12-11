7e1344f6efaab1847d0fd374e2a4fde0f6feaa93
Alexey Smirnov <alexey@gmail.com>
2017-12-11 17:51:58 +0800

    cleanup

--- prayerbook/Db.swift
+++ prayerbook/Db.swift
@@ -11,13 +11,6 @@ import UIKit
 import Squeal
 import swift_toolkit
 
-extension Collection where Indices.Iterator.Element == Index {
-    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Generator.Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
-
 struct Db {
     static let groupURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupId)!
 
