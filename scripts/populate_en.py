b2d4cda10b84c6d960a2732ac8f1d9ea5870f1f2
IY0018 <alexey@ironyun.com>
2019-08-08 15:45:58 +0800

    Added Old Testament in English

--- scripts/populate_en.py
+++ scripts/populate_en.py
@@ -3,48 +3,19 @@ import re
 import pprint as p
 import sqlite3 as lite
 
-bookname = "prov"
+# http://www.gutenberg.org/cache/epub/10/pg10.txt
+
+bookname = "mal"
 
 with open("./%s_en.txt" % bookname, 'r') as content_file, lite.connect("./%s_en.sqlite" % bookname) as con:
     content = content_file.read()
     content = ' '.join(content.split())
-    
-    cur = con.cursor() 
 
+    cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS scripture")
     cur.execute("CREATE TABLE scripture(chapter INT,verse INT, text TEXT)")
 
-    chapter = 1
-    verse = 1
-    while True:
-        
-        pattern = ".*%d:%d (.*) %d:%d " % (chapter, verse, chapter, verse+1)
-        print "%d:%d" % (chapter, verse)
-        old_chapter = chapter
-        old_verse = verse
-                
-        obj = re.match(pattern, content)
-        if not obj:
+    pattern = "(\d+):(\d+) (.*?)(?=\d+:\d+|$)"
 
-            pattern = ".*%d:%d (.*) %d:%d " % (chapter, verse, chapter+1, 1)
-            obj = re.match(pattern, content)
-            if not obj:
-                break
-
-            else:
-                chapter += 1
-                verse = 1
-                
-        else:
-            verse += 1
-
-        cur.execute("INSERT INTO scripture VALUES(%d, %d, \"%s\")" % (old_chapter, old_verse, obj.group(1)))  
-        p.pprint(obj.group(1))
-        
-    
-    pattern = ".*%d:%d (.*)" % (chapter, verse)
-    obj = re.match(pattern, content)
-    if obj:
-        cur.execute("INSERT INTO scripture VALUES(%d, %d, \"%s\")" % (chapter, verse, obj.group(1)))  
-        p.pprint(obj.group(1))
-        
+    for (chapter, verse, txt) in re.findall(pattern, content):
+        cur.execute("INSERT INTO scripture VALUES(%d, %d, \"%s\")" % (int(chapter), int(verse), txt))
