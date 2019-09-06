080a3258d5174bd1eb2f2b2bfcbfebe62744fe61
IY0018 <alexey@ironyun.com>
2019-09-06 12:03:41 +0800

    BasicModel

--- prayerbook/PrayerbookModel.swift
+++ prayerbook/PrayerbookModel.swift
@@ -1,37 +1,21 @@
 //
-//  EucharistModel.swift
+//  PrayerbookModel.swift
 //  ponomar
 //
-//  Created by Alexey Smirnov on 9/5/19.
+//  Created by Alexey Smirnov on 9/6/19.
 //  Copyright © 2019 Alexey Smirnov. All rights reserved.
 //
 
 import UIKit
 import swift_toolkit
 
-class EucharistModel : BookModel {
-    var code: String = "Eucharist"
-    
-    var title: String {
-        get { return Translate.s("Eucharist prayers") }
-    }
-    
-    var mode: BookType = .html
-    
-    var isExpandable = false
-    var hasNavigation = false
-        
+protocol BasicModel  {
+    var data: [[String]] { get }
+    var basename : String { get }
+}
+
+extension BasicModel where Self: BookModel {
     func getSections() -> [String] { return [""] }
-    
-    var data: [[String]] = [
-        ["A Canon of Repentance to our Lord Jesus Christ",
-         "Canon To the Most Holy Mother of God",
-         "Canon To the Guardian Angel",
-         "Order of Pre-communion prayers",
-         "Thanksgiving after Holy Communion"
-        ]]
-    
-    static let shared = EucharistModel()
     
     func getItems(_ section: Int) -> [String] {
         return data[section].map { return Translate.s($0) }
@@ -48,17 +32,43 @@ class EucharistModel : BookModel {
     func getContent(at pos: BookPosition) -> Any? {
         guard let index = pos.index else { return nil }
         
-        let filename = String(format: "prayer_communion_%d_%@.html", index.row, Translate.language)
+        let filename = String(format: "%@_%d_%@.html", basename, index.row, Translate.language)
         let bundleName = Bundle.main.path(forResource: filename, ofType: nil)
         let txt:String! = try? String(contentsOfFile: bundleName!, encoding: String.Encoding.utf8)
         
         return txt
     }
-
+    
     func getBookmark(at pos: BookPosition) -> String { return "" }
     func getBookmarkName(_ bookmark: String) -> String { return "" }
     
     func getNextSection(at pos: BookPosition) -> BookPosition? { return nil }
     func getPrevSection(at pos: BookPosition) -> BookPosition? { return nil }
 }
+
+class EucharistModel2 : BasicModel {
+    var code: String = "Eucharist"
+    var basename: String = "prayer_communion"
+    
+    var mode: BookType = .html
+    var isExpandable = false
+    var hasNavigation = false
+    
+    var title: String {
+        get { return Translate.s("Eucharist prayers") }
+    }
+    
+    var data: [[String]] = [
+        ["A Canon of Repentance to our Lord Jesus Christ",
+         "Canon To the Most Holy Mother of God",
+         "Canon To the Guardian Angel",
+         "Order of Pre-communion prayers",
+         "Thanksgiving after Holy Communion"
+        ]]
+
+    static let shared = EucharistModel2()
+
+}
+
+
 
