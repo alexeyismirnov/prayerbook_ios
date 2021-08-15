5be33f23ab2487999507bb061ad0db7700abb8cc
alexeyismirnov <alexey@gmail.com>
2021-08-15 15:50:21 +0800

    donations added

--- prayerbook/ChurchInfo.swift
+++ prayerbook/ChurchInfo.swift
@@ -7,27 +7,40 @@
 //
 
 import UIKit
+import StoreKit
+
 import swift_toolkit
 
 class ChurchInfo: UITableViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
     
     @IBOutlet weak var appButton: UIButton!
+    @IBOutlet weak var donationButton1: UIButton!
+    @IBOutlet weak var donationButton2: UIButton!
+    @IBOutlet weak var donationButton3: UIButton!
+    @IBOutlet weak var donationButtonOther: UIButton!
     
+    static let productIds: Set<ProductIdentifier> = ["calendar_ru1", "calendar_ru2", "calendar_ru3"]
+    let store = IAPHelper(productIds: ChurchInfo.productIds)
+    var products: [SKProduct] = []
+
     override func viewDidLoad() {
         NotificationCenter.default.addObserver(self, selector: #selector(reload), name: .themeChangedNotification, object: nil)
-                
+        NotificationCenter.default.addObserver(self, selector: #selector(purchaseComplete), name: .IAPHelperPurchaseNotification, object: nil)
+
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100
         
-        appButton.backgroundColor = .clear
-        appButton.layer.cornerRadius = 8
-        appButton.layer.borderWidth = 1
-        appButton.contentEdgeInsets =  UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        
+        makeRoundedCorners(appButton)
+        makeRoundedCorners(donationButton1)
+        makeRoundedCorners(donationButton2)
+        makeRoundedCorners(donationButton3)
+        makeRoundedCorners(donationButtonOther)
 
+        loadProducts()
         reload()
     }
 
@@ -41,6 +54,68 @@ class ChurchInfo: UITableViewController {
         }
     }
     
+    @IBAction func makeDonationOther(_ sender: Any) {
+        showPopup(ChurchDonation())
+    }
+    
+    @IBAction func makeDonation1(_ sender: Any) {
+        store.buyProduct(products[0])
+    }
+    
+    @IBAction func makeDonation2(_ sender: Any) {
+        store.buyProduct(products[1])
+    }
+    
+    @IBAction func makeDonation3(_ sender: Any) {
+        store.buyProduct(products[2])
+    }
+    
+    func loadProducts() {
+        donationButton1.isHidden = true
+        donationButton2.isHidden = true
+        donationButton3.isHidden = true
+        
+        products = []
+        
+        store.requestProducts{ success, products in
+          if success {
+            self.products = products!.sorted {$0.price.floatValue < $1.price.floatValue }
+                        
+            for p in self.products {
+                print("Found product: \(p.productIdentifier) \(p.localizedTitle) \(p.price.intValue)")
+            }
+            
+            DispatchQueue.main.async {
+                
+                self.donationButton1.setTitle("Пожертвовать \(self.products[0].price.intValue) р.", for: .normal)
+                self.donationButton2.setTitle("Пожертвовать \(self.products[1].price.intValue) р.", for: .normal)
+                self.donationButton3.setTitle("Пожертвовать \(self.products[2].price.intValue) р.", for: .normal)
+
+                self.donationButton1.isHidden = false
+                self.donationButton2.isHidden = false
+                self.donationButton3.isHidden = false
+                    
+                self.tableView.reloadData()
+            }
+          }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    @objc func purchaseComplete() {
+        DispatchQueue.main.async() {
+            let alert = UIAlertController(title: Translate.s("Donation"),
+                                          message: Translate.s("Thanks for your donation"),
+                                          preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { _ in
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     @objc func reload() {
         label1.textColor = Theme.textColor
 
@@ -50,15 +125,33 @@ class ChurchInfo: UITableViewController {
         label3.textColor = Theme.textColor
         label3.text = Translate.s("app_info")
         
-        appButton.backgroundColor = .flatWhite()
-        appButton.layer.borderColor = UIColor.gray.cgColor
-        appButton.setTitleColor(.black, for: .normal)
+        label4.textColor = Theme.textColor
+        label4.text = Translate.s("donation_info")
+        
+        updateButtonStyle(appButton)
+        updateButtonStyle(donationButton1)
+        updateButtonStyle(donationButton2)
+        updateButtonStyle(donationButton3)
+        updateButtonStyle(donationButtonOther)
         
         tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
          return UITableView.automaticDimension
+    }
+    
+    func makeRoundedCorners(_ button: UIButton!) {
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 1
+        button.contentEdgeInsets =  UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func updateButtonStyle(_ button: UIButton!) {
+        button.backgroundColor = .flatWhite()
+        button.layer.borderColor = UIColor.gray.cgColor
+        button.setTitleColor(.black, for: .normal)
     }
     
 }
---
+++ prayerbook/IAPHelper.swift
@@ -0,0 +1,120 @@
+//
+//  IAPHelper.swift
+//  ponomar
+//
+//  Created by Alexey Smirnov on 8/15/21.
+//  Copyright © 2021 Alexey Smirnov. All rights reserved.
+//
+
+import StoreKit
+
+public typealias ProductIdentifier = String
+public typealias ProductsRequestCompletionHandler = (_ success: Bool, _ products: [SKProduct]?) -> Void
+
+extension Notification.Name {
+  static let IAPHelperPurchaseNotification = Notification.Name("IAPHelperPurchaseNotification")
+}
+
+open class IAPHelper: NSObject  {
+  
+  private let productIdentifiers: Set<ProductIdentifier>
+  private var purchasedProductIdentifiers: Set<ProductIdentifier> = []
+  private var productsRequest: SKProductsRequest?
+  private var productsRequestCompletionHandler: ProductsRequestCompletionHandler?
+  
+    public init(productIds: Set<ProductIdentifier>) {
+        productIdentifiers = productIds
+        super.init()
+        SKPaymentQueue.default().add(self)
+    }
+
+    public func requestProducts(_ completionHandler: @escaping ProductsRequestCompletionHandler) {
+      productsRequest?.cancel()
+      productsRequestCompletionHandler = completionHandler
+
+      productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
+      productsRequest!.delegate = self
+      productsRequest!.start()
+    }
+
+    public func buyProduct(_ product: SKProduct) {
+      print("Buying \(product.productIdentifier)...")
+      let payment = SKPayment(product: product)
+      SKPaymentQueue.default().add(payment)
+    }
+
+    public class func canMakePayments() -> Bool {
+      return SKPaymentQueue.canMakePayments()
+    }
+    
+}
+
+extension IAPHelper: SKProductsRequestDelegate {
+
+  public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+    print("Loaded list of products...")
+    let products = response.products
+    productsRequestCompletionHandler?(true, products)
+    clearRequestAndHandler()
+  }
+
+  public func request(_ request: SKRequest, didFailWithError error: Error) {
+    print("Failed to load list of products.")
+    print("Error: \(error.localizedDescription)")
+    productsRequestCompletionHandler?(false, nil)
+    clearRequestAndHandler()
+  }
+
+  private func clearRequestAndHandler() {
+    productsRequest = nil
+    productsRequestCompletionHandler = nil
+  }
+}
+
+extension IAPHelper: SKPaymentTransactionObserver {
+
+  public func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+    for transaction in transactions {
+      switch (transaction.transactionState) {
+      case .purchased:
+        complete(transaction: transaction)
+        break
+      case .failed:
+        fail(transaction: transaction)
+        break
+      case .restored:
+        break
+      case .deferred:
+        break
+      case .purchasing:
+        break
+      }
+    }
+  }
+
+  private func complete(transaction: SKPaymentTransaction) {
+    print("complete...")
+    deliverPurchaseNotificationFor(identifier: transaction.payment.productIdentifier)
+    SKPaymentQueue.default().finishTransaction(transaction)
+  }
+
+  private func fail(transaction: SKPaymentTransaction) {
+    print("fail...")
+    if let transactionError = transaction.error as NSError?,
+      let localizedDescription = transaction.error?.localizedDescription,
+        transactionError.code != SKError.paymentCancelled.rawValue {
+        print("Transaction Error: \(localizedDescription)")
+      }
+
+    SKPaymentQueue.default().finishTransaction(transaction)
+  }
+
+  private func deliverPurchaseNotificationFor(identifier: String?) {
+    guard let identifier = identifier else { return }
+
+    purchasedProductIdentifiers.insert(identifier)
+    UserDefaults.standard.set(true, forKey: identifier)
+    NotificationCenter.default.post(name: .IAPHelperPurchaseNotification, object: identifier)
+  }
+}
+
