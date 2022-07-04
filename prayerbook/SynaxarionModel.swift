d25a55585aa9b287df1f0ca76379087526c938da
alexeyismirnov <alexey@gmail.com>
2022-07-04 10:51:02 +0800

    Synaxarion fixed

--- prayerbook/SynaxarionModel.swift
+++ prayerbook/SynaxarionModel.swift
@@ -24,13 +24,14 @@ class SynaxarionModel : EbookModel, PreachmentModel {
             cal.greatLentStart-22.days,cal.greatLentStart-15.days,cal.greatLentStart-9.days,cal.greatLentStart-8.days,
             cal.greatLentStart-2.days,cal.greatLentStart-1.days,cal.greatLentStart+5.days,cal.greatLentStart+6.days,
             cal.greatLentStart+13.days,cal.greatLentStart+20.days,cal.greatLentStart+27.days,cal.greatLentStart+31.days,
-            cal.greatLentStart+33.days,
+            cal.greatLentStart+33.days, cal.greatLentStart+34.days,
+
             cal.pascha-8.days,cal.pascha-7.days,cal.pascha-6.days,
             cal.pascha-5.days,cal.pascha-4.days,cal.pascha-3.days,cal.pascha-2.days,
             cal.pascha-1.days,
             cal.pascha, cal.pascha+5.days,cal.pascha+7.days,cal.pascha+14.days,
-            cal.pascha+21.days, cal.pascha+24.days,cal.pascha+28.days,cal.pascha+35.days,
-            cal.pascha+42.days,cal.pascha+39.days,cal.pascha+49.days, cal.pascha+50.days, cal.pascha+56.days,
+            cal.pascha+21.days, cal.pascha+24.days,cal.pascha+28.days,cal.pascha+35.days, cal.pascha+39.days,
+            cal.pascha+42.days,cal.pascha+49.days, cal.pascha+50.days, cal.pascha+56.days,
         ]
         
         if let index = dates.firstIndex(of: date) {
