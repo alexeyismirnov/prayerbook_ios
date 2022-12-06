2b64e9386b8e7dae1fd1ef352426d2ff5abb91d5
alexeyismirnov <alexey@gmail.com>
2022-12-06 11:59:25 +0800

    carthage fixed

---
+++ carthage.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+carthage build --platform iOS --use-xcframeworks
+
+
---
+++ scripts/populate_hk.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sqlite3 as lite
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+book = 4
+num_chapters = 50
+
+with lite.connect("./%s.sqlite" % book) as con:
+
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS scripture")
+    cur.execute("CREATE TABLE scripture(chapter INT,verse INT, text TEXT)")
+
+    for chapter in range(1, num_chapters+1):
+        page = urllib2.urlopen("http://www.sbofmhk.org/pub/body/cpray/c1_online_bible/sbbible/sbofm_bible_content.php?book=%d&chapter=%d" % (book,chapter)).read()
+        soup = BeautifulSoup(page, "html.parser")
+
+        for tr in soup.find_all("tr"):
+            children = tr.find_all("td", recursive=False)
+
+            if len(children) != 3 or len(children[0].getText()) == 0:
+                continue
+
+            m = re.search("(\d+):(\d+)", children[0].getText())
+            if m is None:
+                continue
+
+            # print children[0].getText()
+            chapter,verse = children[0].getText().split(":")
+            verse = verse.split("-")[0]
+            verse = verse.split(",")[0]
+
+            print "%d:%d" % ( int(chapter), int(verse))
+            print children[1].getText()
+
+            cur.execute("INSERT INTO scripture VALUES(%d, %d, \"%s\")" % (int(chapter), int(verse), children[1].getText()))
