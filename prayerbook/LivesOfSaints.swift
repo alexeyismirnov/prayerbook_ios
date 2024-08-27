4ff466ac2bbb722b4c62c557189d390073aae1fd
Alexey Smirnov <alexey@gmail.com>
2024-08-27 13:40:58 +0800

    lives of saints (epubs) added

--- prayerbook/LivesOfSaints.swift
+++ prayerbook/LivesOfSaints.swift
@@ -38,25 +38,22 @@ private class SaintsCalendar {
         let greatLentStart = pascha - 48.days
         
         day("holyFathersSixCouncils").date = Cal.nearestSunday(Date(29, 7, year))
-        // day("findingOfHead").date = isLeapYear ? Date(8, 3, year) : Date(9, 3, year)
+        day("findingOfHead").date = isLeapYear ? Date(8, 3, year) : Date(9, 3, year)
         
+        day("saturdayOfFathers").date =  greatLentStart - 2.days
+        day("sunday4GreatLent").date =  greatLentStart + 27.days
+
         day("greatMonday").date = pascha - 6.days
         day("greatTuesday").date = pascha - 5.days
         // day("greatWednesday").date = pascha - 4.days
         day("greatSaturday").date = pascha - 1.days
-        
-        // day("beginningOfGreatLent").date = greatLentStart
-        // day("saturday1GreatLent").date = greatLentStart + 5.days
-        // day("sunday1GreatLent").date = greatLentStart + 6.days
-        // day("sunday3GreatLent").date = greatLentStart + 20.days
-        // day("sunday5GreatLent").date = greatLentStart + 34.days
-        // day("palmSunday").date = pascha - 7.days
 
         day("ascension").date = pascha + 39.days
         day("pentecost").date = pentecost
         day("sunday1AfterPentecost").date = pentecost + 7.days
 
         day("sunday3AfterPascha").date = pascha + 14.days
+        day("sunday4AfterPascha").date = pascha + 21.days
         day("sunday7AfterPascha").date = pascha + 42.days
         
         day("kurskTheotokos").date = pentecost+12.days
